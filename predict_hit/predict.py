c3e8940915ae8f050765a28ee8ce60b79924cad8
Peter870512 <b05901070@ntu.edu.tw>
2020-08-04 18:20:48 +0800

    the function of hitting result prediction

---
+++ predict_hit/predict.py
@@ -0,0 +1,79 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+import time
+
+class Predicter(nn.Module):
+    def __init__(self):
+        super(Predicter, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(3, 32),
+            nn.ReLU(),
+            nn.Linear(32, 64),
+            nn.ReLU(),
+            nn.Linear(64, 256),
+            nn.ReLU(),
+            nn.Linear(256, 7),
+        )
+
+    def forward(self, x):
+        out = self.fc(x)
+        return out
+
+class testDataset(Dataset):
+    def __init__(self, x):
+        self.x = torch.Tensor(x)
+    def __len__(self):
+        return len(self.x)
+    def __getitem__(self, index):
+        X = self.x[index]
+        return X
+
+def prediction_of_hit(exit_velo, launch_angle, spray_angle):
+    start_time = time.time()
+    model_path = 'predictor.pth'
+    if torch.cuda.is_available():
+        model = Predicter.cuda()
+        model.load_state_dict(torch.load(model_path))
+    else:
+        model = Predicter()
+        model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.eval()
+    data = np.zeros((1,3))
+    data[0, 0] = exit_velo
+    data[0, 1] = launch_angle
+    data[0, 2] = spray_angle
+    data = testDataset(data)
+    batch_size = 1
+    test_loader = DataLoader(data, batch_size=batch_size, shuffle=False)
+    for i, data in enumerate(test_loader):
+        if torch.cuda.is_available():
+            output = model(data.cuda())
+        else:
+            output = model(data)
+    output = np.argmax(output.cpu().data.numpy(), axis=1)
+
+    predict_result = None
+    if output == 0:
+        predict_result = 'single'
+    elif output == 1:
+        predict_result = 'double'
+    elif output == 2:
+        predict_result = 'triple'
+    elif output == 3:
+        predict_result = 'homerun'
+    elif output == 4:
+        predict_result = 'fly out'
+    elif output == 5:
+        predict_result = 'ground out'
+    elif output == 6:
+        predict_result = 'line out'
+    end_time = time.time()
+    print(end_time - start_time)  
+    print(predict_result)
+    return predict_result
+
+if __name__ == '__main__':
+    prediction_of_hit(80, 1, 30)
